5e9a9d876b275a83262f7a338027c98fcb75c596
Eric Smith <esmith@8thlight.com>
2016-04-27 22:03:36 -0500

    Install phantomjs/jspm as needed
    Makes the setup simpler.

--- jspm/setup.sh
+++ jspm/setup.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# Check for Node, JSPM, and Phantomjs
+# Check for Node
+if ! hash npm 2>/dev/null; then
+  echo "This setup requires Node.js, for a CLI. I will not install this for you, because it's insane. Head to nodejs.org and go get it."
+  exit
+fi
+
+# Check for phantomjs
+if ! hash phantomjs 2>/dev/null; then
+  echo "Phantomjs is not present. Do you want me to install it for you? It will be installed globally. (y/n)"
+  read Answer
+  if [ $Answer = "y" ]; then
+    npm install phantomjs-prebuilt -g
+  fi
+fi
+
+# Check for jspm
+if ! hash jspm 2>/dev/null; then
+  echo "JSPM is not present. Do you want me to install it for you? It will be installed globally. (y/n)"
+  read Answer
+  if [ $Answer = "y" ]; then
+    npm install jspm -g
+  fi
+fi
 
 # Ask for the name of the name of the project
 echo "Enter Project Name:"
